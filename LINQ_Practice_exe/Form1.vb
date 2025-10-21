bc917820b49fca75caa1e705227b19c8171b5e66
AdiSelabino <floresadrian3127@gmail.com>
2025-10-21 19:36:40 +0800

    Add project files.

---
+++ LINQ_Practice_exe/Form1.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Linq
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim input As String = TextBox1.Text
+        Dim items = input.Split(","c)
+
+
+        Dim result = items.Distinct().OrderByDescending(Function(x) x.Trim())
+
+
+        Label1.Text = "Sorted Unique Products:" & vbCrLf & String.Join(vbCrLf, result)
+    End Sub
+End Class
---
+++ LINQ_Practice_exe/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("LINQ_Practice_exe")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("LINQ_Practice_exe")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("ecb8c8f6-64c8-4eb8-89da-d2eca3f485e2")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
